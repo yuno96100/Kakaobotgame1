3845e7e5f0ecad6b862513fa1a4851b94d8c73db
yuno96100 <cyo579@gmail.com>
2026-01-22 00:28:45 +0900

    Add Champions module with character roles

---
+++ modules/champions.js
@@ -0,0 +1,11 @@
+const Champions = {
+    // 100번대: 전사 / 200번대: 탱커 / 300번대: 마법사 등...
+    101: { name: "전사1", role: "전사" },
+    201: { name: "탱커1", role: "탱커" },
+    301: { name: "마법사1", role: "마법사" },
+    401: { name: "암살자1", role: "암살자" },
+    501: { name: "원거리딜러1C", role: "원거리딜러" },
+    601: { name: "서포터1", role: "서포터" }
+};
+
+module.exports = Champions;
