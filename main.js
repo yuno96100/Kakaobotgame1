660c636c01b41a297238589648482efb5ecd1521
yuno96100 <cyo579@gmail.com>
2026-01-23 00:09:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-// main.js 파일 (또는 스크립트 메인 부분)
+// sdcard/msgbot/Bots/sub/main.js
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     if (msg.startsWith(".")) {
-        // ⭐ 이 순서를 반드시 지켜야 합니다! (5개 인자)
-        // Handler.process(방이름, 메세지, 보낸이, 단톡여부, 응답기)
+        // [순서 엄격 준수] room(1), msg(2), sender(3), isGroupChat(4), replier(5)
         Handler.process(room, msg, sender, isGroupChat, replier);
     }
 }
