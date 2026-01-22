624842f61fa3bb01860fa247809410f6add2e121
yuno96100 <cyo579@gmail.com>
2026-01-22 23:36:58 +0900

    Refactor response function to use handler module

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function response(room, msg, sender, isGroupChat, replier) {
-    if (msg === "테스트") {
-        replier.reply("GitHub 연결 성공!");
+const Handler = require("./modules/handler");
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    if (msg.startsWith(".")) {
+        // ⭐ 중요: room과 isGroupChat을 반드시 포함해서 5개를 넘겨야 합니다.
+        Handler.process(room, msg, sender, isGroupChat, replier);
     }
 }
