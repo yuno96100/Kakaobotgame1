2de8f4f7170cdb4f1afbefbb572f0ad30ed4ce9c
yuno96100 <cyo579@gmail.com>
2026-01-23 02:51:04 +0900

    Update admin.js

--- modules/admin.js
+++ modules/admin.js
@@ -4,16 +4,14 @@ const AdminDB = {};
 AdminDB.getAdmins = function() {
     try {
         if (!java.io.File(ADMIN_PATH).exists()) {
-            var initial = ["관리자"];
+            var initial = ["관리자"]; 
             var folder = new java.io.File("sdcard/msgbot/Bots/sub/data/");
             if (!folder.exists()) folder.mkdirs();
             FileStream.write(ADMIN_PATH, JSON.stringify(initial));
             return initial;
         }
         return JSON.parse(FileStream.read(ADMIN_PATH));
-    } catch (e) {
-        return ["관리자"];
-    }
+    } catch (e) { return ["관리자"]; }
 };
 
 AdminDB.add = function(name) {
