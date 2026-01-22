58b2c686532949cc7f179f1c24154df0820ebdb5
yuno96100 <cyo579@gmail.com>
2026-01-23 02:34:57 +0900

    Update user.js

--- modules/user.js
+++ modules/user.js
@@ -1,21 +1,18 @@
+// modules/user.js (롤 컨셉 튜닝)
 const UserDB = {};
 const DATA_PATH = "sdcard/msgbot/Bots/sub/data/";
 
-// 데이터 저장용 폴더 자동 생성
-var folder = new java.io.File(DATA_PATH);
-if (!folder.exists()) folder.mkdirs();
-
-// 유저 데이터 불러오기 (없으면 신규 생성)
 UserDB.get = function(sender) {
     var path = DATA_PATH + sender + ".json";
     if (!java.io.File(path).exists()) {
         var newUser = {
             name: sender,
-            level: 1,
-            gold: 100,
-            hp: 100,
-            exp: 0,
-            lastStock: "" // 출석 체크 시간 저장용
+            lv: 1,           // 계정 레벨
+            exp: 0,          // 계정 경험치
+            money: 500,      // 보유 재화
+            win: 0,
+            loss: 0,
+            chars: ["전사"]   // 기본 캐릭터
         };
         this.save(sender, newUser);
         return newUser;
@@ -23,7 +20,6 @@ UserDB.get = function(sender) {
     return JSON.parse(FileStream.read(path));
 };
 
-// 유저 데이터 저장하기
 UserDB.save = function(sender, data) {
     FileStream.write(DATA_PATH + sender + ".json", JSON.stringify(data, null, 4));
 };
