f52f035e2831bb9b139fdd244c7fdf315ef06f03
yuno96100 <cyo579@gmail.com>
2026-01-23 02:29:42 +0900

    Update user.js

--- modules/user.js
+++ modules/user.js
@@ -1,12 +1,27 @@
-module.exports = {
-    get: function(name) {
-        var path = "/sdcard/msgbot/Bots/sub/data/" + name + ".json";
-        if (java.io.File(path).exists()) return JSON.parse(FileStream.read(path));
-        return { name: name, level: 1, exp: 0, maxExp: 100, money: 1000, lastAttendance: "" };
-    },
-    save: function(name, data) {
-        var folder = new java.io.File("/sdcard/msgbot/Bots/sub/data/");
-        if (!folder.exists()) folder.mkdirs();
-        FileStream.write("/sdcard/msgbot/Bots/sub/data/" + name + ".json", JSON.stringify(data, null, 4));
+const UserDB = {};
+const DATA_PATH = "sdcard/msgbot/Bots/sub/data/";
+
+// 데이터 저장용 폴더 생성
+java.io.File(DATA_PATH).mkdirs();
+
+UserDB.get = function(sender) {
+    var path = DATA_PATH + sender + ".json";
+    if (!FileStream.exists(path)) {
+        // 신규 유저 초기 데이터
+        var newUser = {
+            name: sender,
+            level: 1,
+            gold: 100,
+            hp: 100
+        };
+        this.save(sender, newUser);
+        return newUser;
     }
+    return JSON.parse(FileStream.read(path));
 };
+
+UserDB.save = function(sender, data) {
+    FileStream.write(DATA_PATH + sender + ".json", JSON.stringify(data, null, 4));
+};
+
+module.exports = UserDB;
