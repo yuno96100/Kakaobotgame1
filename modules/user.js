0c3b3112ad4d1f56bfba8006d9f1b59889bea2b5
yuno96100 <cyo579@gmail.com>
2026-01-23 02:16:06 +0900

    Update user.js

--- modules/user.js
+++ modules/user.js
@@ -1,32 +1,12 @@
-const cache = {};
-
 module.exports = {
     get: function(name) {
-        if (cache[name]) return cache[name];
-        var path = "sdcard/msgbot/Bots/sub/data/" + name + ".json";
-        if (java.io.File(path).exists()) {
-            var data = JSON.parse(FileStream.read(path));
-            cache[name] = data;
-            return data;
-        }
-        return { name: name, level: 1, exp: 0, maxExp: 100, money: 1000, win: 0, loss: 0, roomName: "", lastAttendance: "" };
+        var path = "/sdcard/msgbot/Bots/sub/data/" + name + ".json";
+        if (java.io.File(path).exists()) return JSON.parse(FileStream.read(path));
+        return { name: name, level: 1, exp: 0, maxExp: 100, money: 1000, lastAttendance: "" };
     },
     save: function(name, data) {
-        cache[name] = data;
-        var folder = new java.io.File("sdcard/msgbot/Bots/sub/data/");
+        var folder = new java.io.File("/sdcard/msgbot/Bots/sub/data/");
         if (!folder.exists()) folder.mkdirs();
-        FileStream.write("sdcard/msgbot/Bots/sub/data/" + name + ".json", JSON.stringify(data, null, 4));
-    },
-    checkLevelUp: function(user) {
-        if (user.exp >= user.maxExp) {
-            user.level++;
-            user.exp -= user.maxExp;
-            user.maxExp = Math.floor(user.maxExp * 1.5);
-            return true;
-        }
-        return false;
-    },
-    clearCache: function() {
-        for (var key in cache) delete cache[key];
+        FileStream.write("/sdcard/msgbot/Bots/sub/data/" + name + ".json", JSON.stringify(data, null, 4));
     }
 };
