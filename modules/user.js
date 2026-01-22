e7fbb46b52a9aa1e8721af223f094f89c6487b7c
yuno96100 <cyo579@gmail.com>
2026-01-22 10:26:40 +0900

    user.js 업데이트

--- modules/user.js
+++ modules/user.js
@@ -1,15 +1,13 @@
 const USER_PATH = "sdcard/msgbot/Bots/sub/data/";
-
 const UserDB = {};
 
-// 레벨업 체크 및 보상 로직
 UserDB.checkLevelUp = function(userData) {
     var leveledUp = false;
     while (userData.exp >= userData.maxExp) {
         userData.exp -= userData.maxExp;
         userData.level++;
-        userData.maxExp = Math.floor(userData.maxExp * 1.2); // 다음 레벨 요구치 20% 증가
-        userData.money += 500; // 레벨업 보상 500G
+        userData.maxExp = Math.floor(userData.maxExp * 1.2);
+        userData.money += 500;
         leveledUp = true;
     }
     return leveledUp;
@@ -19,38 +17,17 @@ UserDB.get = function(name) {
     try {
         var file = new java.io.File(USER_PATH + name + ".json");
         var userData;
-
         if (!file.exists()) {
-            var folder = new java.io.File(USER_PATH);
-            if (!folder.exists()) folder.mkdirs();
-
-            userData = {
-                name: name,
-                level: 1,
-                exp: 0,
-                maxExp: 100,
-                money: 1000,
-                win: 0,
-                loss: 0,
-                ownedChars: [101],
-                lastAttendance: ""
-            };
+            userData = { name: name, level: 1, exp: 0, maxExp: 100, money: 1000, win: 0, loss: 0, ownedChars: [101], lastAttendance: "" };
             this.save(name, userData);
         } else {
             userData = JSON.parse(FileStream.read(USER_PATH + name + ".json"));
-            
-            // 데이터 필드 자동 보정 (Schema 보정)
-            var updated = false;
-            if (userData.exp === undefined) { userData.exp = 0; updated = true; }
-            if (userData.maxExp === undefined) { userData.maxExp = 100; updated = true; }
-            if (userData.lastAttendance === undefined) { userData.lastAttendance = ""; updated = true; }
-            
-            if (updated) this.save(name, userData);
+            if (userData.exp === undefined) userData.exp = 0;
+            if (userData.maxExp === undefined) userData.maxExp = 100;
+            if (userData.lastAttendance === undefined) userData.lastAttendance = "";
         }
         return userData;
-    } catch (e) {
-        return null;
-    }
+    } catch (e) { return null; }
 };
 
 UserDB.save = function(name, data) {
