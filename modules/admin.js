6092b8e82ba8c28c9ae1860d0a91def90104c85f
yuno96100 <cyo579@gmail.com>
2026-01-23 00:59:50 +0900

    Update admin.js

--- modules/admin.js
+++ modules/admin.js
@@ -1,40 +1,27 @@
-const ADMIN_PATH = "sdcard/msgbot/Bots/sub/data/admins.json";
 const AdminDB = {};
+const path = "sdcard/msgbot/Bots/sub/modules/admin_list.json";
 
 AdminDB.getAdmins = function() {
-    try {
-        if (!java.io.File(ADMIN_PATH).exists()) {
-            var initial = ["관리자"];
-            var folder = new java.io.File("sdcard/msgbot/Bots/sub/data/");
-            if (!folder.exists()) folder.mkdirs();
-            FileStream.write(ADMIN_PATH, JSON.stringify(initial));
-            return initial;
-        }
-        return JSON.parse(FileStream.read(ADMIN_PATH));
-    } catch (e) {
-        return ["관리자"];
-    }
+    if (!java.io.File(path).exists()) return ["관리자"];
+    return JSON.parse(FileStream.read(path));
 };
 
 AdminDB.add = function(name) {
-    var admins = this.getAdmins();
-    if (admins.indexOf(name) === -1) {
-        admins.push(name);
-        FileStream.write(ADMIN_PATH, JSON.stringify(admins));
-        return true;
-    }
-    return false;
+    var list = this.getAdmins();
+    if (list.indexOf(name) > -1) return false;
+    list.push(name);
+    FileStream.write(path, JSON.stringify(list));
+    return true;
 };
 
 AdminDB.remove = function(name) {
-    var admins = this.getAdmins();
-    var index = admins.indexOf(name);
-    if (index > -1 && name !== "관리자") {
-        admins.splice(index, 1);
-        FileStream.write(ADMIN_PATH, JSON.stringify(admins));
-        return true;
-    }
-    return false;
+    if (name === "관리자") return false;
+    var list = this.getAdmins();
+    var idx = list.indexOf(name);
+    if (idx === -1) return false;
+    list.splice(idx, 1);
+    FileStream.write(path, JSON.stringify(list));
+    return true;
 };
 
 module.exports = AdminDB;
