8924fa98f9f0bfbc7238a4a69b22676bc661ecd9
yuno96100 <cyo579@gmail.com>
2026-01-23 02:43:41 +0900

    Update user.js

--- modules/user.js
+++ modules/user.js
@@ -2,33 +2,42 @@ const USER_PATH = "sdcard/msgbot/Bots/sub/data/";
 
 const UserDB = {};
 
-// 유저 데이터 불러오기 및 생성
 UserDB.get = function(name) {
     var file = new java.io.File(USER_PATH + name + ".json");
+    var userData;
+
     if (!file.exists()) {
-        // 폴더가 없으면 생성
         var folder = new java.io.File(USER_PATH);
         if (!folder.exists()) folder.mkdirs();
 
-        // 신규 유저 초기 데이터
-        var newUser = {
+        userData = {
             name: name,
             level: 1,
+            exp: 0,
+            maxExp: 100,
             money: 1000,
             win: 0,
             loss: 0,
-            ownedChars: [101], // 기본 캐릭터 (연습용 전사) 지급
+            ownedChars: [101],
             lastAttendance: ""
         };
-        this.save(name, newUser);
-        return newUser;
+        this.save(name, userData);
+    } else {
+        userData = JSON.parse(FileStream.read(USER_PATH + name + ".json"));
+        
+        // [보강: 필드 자동 보정] 새로운 항목이 추가되어도 에러 안 나게 방지
+        var updated = false;
+        if (userData.exp === undefined) { userData.exp = 0; updated = true; }
+        if (userData.maxExp === undefined) { userData.maxExp = 100; updated = true; }
+        if (userData.lastAttendance === undefined) { userData.lastAttendance = ""; updated = true; }
+        
+        if (updated) this.save(name, userData);
     }
-    return JSON.parse(FileStream.read(USER_PATH + name + ".json"));
+    return userData;
 };
 
-// 유저 데이터 저장
 UserDB.save = function(name, data) {
     FileStream.write(USER_PATH + name + ".json", JSON.stringify(data));
 };
 
-module.exports = UserDB; // 이 부분이 있어야 Handler에서 get 함수를 인식합니다.
+module.exports = UserDB;
