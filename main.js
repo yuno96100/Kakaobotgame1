0016cd79e4fbe27b6fc700d9a1160e592d1c12dd
yuno96100 <cyo579@gmail.com>
2026-01-22 23:58:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const Handler = require("./modules/handler");
+// main.js 파일 (또는 스크립트 메인 부분)
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     if (msg.startsWith(".")) {
-        // ⭐ 중요: room과 isGroupChat을 반드시 포함해서 5개를 넘겨야 합니다.
+        // ⭐ 이 순서를 반드시 지켜야 합니다! (5개 인자)
+        // Handler.process(방이름, 메세지, 보낸이, 단톡여부, 응답기)
         Handler.process(room, msg, sender, isGroupChat, replier);
     }
 }
