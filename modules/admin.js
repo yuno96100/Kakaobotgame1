ce9508348681d5ff5fc67783020dd89dcb6674a4
yuno96100 <cyo579@gmail.com>
2026-01-23 02:41:07 +0900

    Update admin.js

--- modules/admin.js
+++ modules/admin.js
@@ -1,21 +1,19 @@
-// /sdcard/msgbot/Bots/sub/modules/admin.js
-const ADMIN_PATH = "/sdcard/msgbot/Bots/sub/data/admins.json";
+const ADMIN_PATH = "sdcard/msgbot/Bots/sub/data/admins.json";
+
 const AdminDB = {};
 
 AdminDB.getAdmins = function() {
     try {
-        var file = new java.io.File(ADMIN_PATH);
-        if (!file.exists()) {
-            var initial = ["관리자"];
-            var folder = new java.io.File("/sdcard/msgbot/Bots/sub/data/");
+        if (!java.io.File(ADMIN_PATH).exists()) {
+            var initial = ["관리자"]; // 기본 관리자 설정
+            var folder = new java.io.File("sdcard/msgbot/Bots/sub/data/");
             if (!folder.exists()) folder.mkdirs();
             FileStream.write(ADMIN_PATH, JSON.stringify(initial));
             return initial;
         }
         return JSON.parse(FileStream.read(ADMIN_PATH));
     } catch (e) {
-        Log.error("AdminDB 로드 에러: " + e.message);
-        return ["관리자"]; // 에러 시 기본 관리자 반환
+        return ["관리자"];
     }
 };
 
@@ -32,7 +30,6 @@ AdminDB.add = function(name) {
 AdminDB.remove = function(name) {
     var admins = this.getAdmins();
     var index = admins.indexOf(name);
-    // '관리자'라는 기본 이름은 삭제 불가능하게 설정
     if (index > -1 && name !== "관리자") {
         admins.splice(index, 1);
         FileStream.write(ADMIN_PATH, JSON.stringify(admins));
