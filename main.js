d8ad3c8bea839ececaa01bf3f86b03510eb33eaf
yuno96100 <cyo579@gmail.com>
2026-01-23 01:04:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const Handler = require("./modules/handler");
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     if (msg.startsWith(".")) {
-        // 단톡/개인톡 구분 없이 모든 메시지를 핸들러로 전달
         Handler.process(room, msg, sender, isGroupChat, replier);
     }
 }
