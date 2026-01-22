5cdc4d574318cc80b260da56d41f27cb8d724a22
yuno96100 <cyo579@gmail.com>
2026-01-23 02:41:35 +0900

    Update user.js

--- modules/user.js
+++ modules/user.js
@@ -1,29 +1,34 @@
+const USER_PATH = "sdcard/msgbot/Bots/sub/data/";
+
 const UserDB = {};
-const DATA_PATH = "sdcard/msgbot/Bots/sub/data/";
 
-var folder = new java.io.File(DATA_PATH);
-if (!folder.exists()) folder.mkdirs();
+// 유저 데이터 불러오기 및 생성
+UserDB.get = function(name) {
+    var file = new java.io.File(USER_PATH + name + ".json");
+    if (!file.exists()) {
+        // 폴더가 없으면 생성
+        var folder = new java.io.File(USER_PATH);
+        if (!folder.exists()) folder.mkdirs();
 
-UserDB.get = function(sender) {
-    var path = DATA_PATH + sender + ".json";
-    if (!java.io.File(path).exists()) {
+        // 신규 유저 초기 데이터
         var newUser = {
-            name: sender,
-            title: "초보 소환사",      // 칭호 추가
-            level: 1,                // 계정 레벨
-            money: 1000,             // 구매 재화
-            win: 0,                  // 승리
-            loss: 0,                 // 패배
-            ownedChars: ["기본 전사"] // 보유 캐릭터
+            name: name,
+            level: 1,
+            money: 1000,
+            win: 0,
+            loss: 0,
+            ownedChars: [101], // 기본 캐릭터 (연습용 전사) 지급
+            lastAttendance: ""
         };
-        this.save(sender, newUser);
+        this.save(name, newUser);
         return newUser;
     }
-    return JSON.parse(FileStream.read(path));
+    return JSON.parse(FileStream.read(USER_PATH + name + ".json"));
 };
 
-UserDB.save = function(sender, data) {
-    FileStream.write(DATA_PATH + sender + ".json", JSON.stringify(data, null, 4));
+// 유저 데이터 저장
+UserDB.save = function(name, data) {
+    FileStream.write(USER_PATH + name + ".json", JSON.stringify(data));
 };
 
-module.exports = UserDB;
+module.exports = UserDB; // 이 부분이 있어야 Handler에서 get 함수를 인식합니다.
