83bbbdcdcc0b6c12cf633856580609ce818d4d44
yuno96100 <cyo579@gmail.com>
2026-01-23 00:59:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-// sdcard/msgbot/Bots/sub/main.js
 const Handler = require("./modules/handler");
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     if (msg.startsWith(".")) {
-        // [순서 주의] 1.방이름, 2.메세지, 3.보낸사람, 4.단톡여부, 5.응답기
+        // 단톡/개인톡 구분 없이 모든 메시지를 핸들러로 전달
         Handler.process(room, msg, sender, isGroupChat, replier);
     }
 }
