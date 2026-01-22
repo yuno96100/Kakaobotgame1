ee578a937d04cf2385a8e2848571b9d933074e59
yuno96100 <cyo579@gmail.com>
2026-01-23 02:35:17 +0900

    Update user.js

--- modules/user.js
+++ modules/user.js
@@ -1,18 +1,20 @@
-// modules/user.js (롤 컨셉 튜닝)
 const UserDB = {};
 const DATA_PATH = "sdcard/msgbot/Bots/sub/data/";
+
+var folder = new java.io.File(DATA_PATH);
+if (!folder.exists()) folder.mkdirs();
 
 UserDB.get = function(sender) {
     var path = DATA_PATH + sender + ".json";
     if (!java.io.File(path).exists()) {
         var newUser = {
             name: sender,
-            lv: 1,           // 계정 레벨
-            exp: 0,          // 계정 경험치
-            money: 500,      // 보유 재화
-            win: 0,
-            loss: 0,
-            chars: ["전사"]   // 기본 캐릭터
+            title: "초보 소환사",      // 칭호 추가
+            level: 1,                // 계정 레벨
+            money: 1000,             // 구매 재화
+            win: 0,                  // 승리
+            loss: 0,                 // 패배
+            ownedChars: ["기본 전사"] // 보유 캐릭터
         };
         this.save(sender, newUser);
         return newUser;
