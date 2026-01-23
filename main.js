b359359610c5ebea574766e7ccf81856ab4d4beb
yuno96100 <cyo579@gmail.com>
2026-01-23 14:29:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-const Handler = require("./modules/handler");
-
-
-
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-
-    if (msg === "/업데이트" && sender === "관리자") {
-
-        updateBot(replier); // 봇 소스 갱신 함수
-
-        return;
-
+    if (msg === ".업데이트" && sender === "관리자") {
+        // 서브폰의 코드를 깃허브에서 갱신하라는 신호를 보내거나 
+        // 메인폰 자체의 관리 로직을 업데이트합니다.
+        replier.reply("✅ 관리자 시스템 업데이트 신호를 송신합니다.");
     }
-
-    Handler.process(msg, sender, replier);
-
 }
