22627af4b9e23ac555d474132792a5dacef83be5
yuno96100 <cyo579@gmail.com>
2026-01-25 16:17:36 +0900

    room_manager.js 업데이트

--- modules/room_manager.js
+++ modules/room_manager.js
@@ -1,4 +1,3 @@
-// [modules/room_manager.js]
 const RoomManager = {};
 const ROOM_DIR = "/sdcard/msgbot/Bots/sub/data/rooms/";
 
@@ -10,18 +9,11 @@ RoomManager.saveRoom = function(roomName, roomHash) {
     var roomData = {
         "roomName": roomName,
         "roomHash": roomHash,
-        "lastSeen": new Date().toLocaleString()
+        "lastUpdate": new Date().toLocaleString()
     };
     
-    // 방 정보가 없거나 이름이 바뀌었을 경우 업데이트 저장
     FileStream.write(path, JSON.stringify(roomData, null, 4));
     return roomData;
-};
-
-RoomManager.getRoomData = function(roomHash) {
-    var path = ROOM_DIR + roomHash + ".json";
-    if (!new java.io.File(path).exists()) return null;
-    return JSON.parse(FileStream.read(path));
 };
 
 module.exports = RoomManager;
